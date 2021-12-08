8dc2aaf53a93353d06b1c06428f0e076e26d92c8
Fatih Topçu <FatihTopcu97@gmail.com>
2021-12-09 02:50:15 +0300

    Added payments and client side

---
+++ client/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./SurveyNew";
+import Landing from "./Landing";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class App extends Component {
+
+  componentDidMount(){
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/surveys" element={<Dashboard />} />
+            <Route path="/surveys/new" element={<SurveyNew />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
